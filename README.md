8b16474797c9b300b1dae8119a6a083edeacb7f7
Daniel Xu <xudh47@gmail.com>
2022-08-07 22:46:16 -0700

    Updated readme and updated local storage so the to-do list does not disappear when the page is refreshed.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Work-Day-Scheduler
+
+## Description
+
+This project was built using jQuery and Bootstrap. The purpose of this project was to further enhance DOM and API knowledge as well as getting familiar with jQuery and Bootstrap.
+
+## Screenshot
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var currentDayEl = $("#currentDay");
-var timeBlockEl = $("time-block");
+var timeBlockEl = $(".time-block");
 
+console.log(timeBlockEl);
 // MM DD YY Display
 currentDayEl.text(moment().format("LLLL"));
 
@@ -10,35 +11,49 @@ var saveBtn = $(".saveBtn");
 // How to make submit button independent from each event?
 $(document).ready(function () {
   $(".saveBtn").on("click", function () {
-    // function that receives textarea
-    // Need a if / else statement that only does this under some conditions
-
-    // if descrption content
-    //saves in local storage
+    // Value from textarea
     var value = $(this).siblings(".description").val();
+    console.log(value);
     var time = $(this).parent().attr("id");
+    console.log(time);
 
+    //saves in local storage
     localStorage.setItem(time, value);
-
-    function hourUpdated() {
-      //loop over time blocks
-      var currentHour = moment().hours();
-      $(".time-block").each(function () {
-        //check if we have moved to the next hour
-        var blockhour = parseInt($(this).attr("id").split("-")[1]);
-
-        if (blockhour < currentHour) {
-          $(this).addClass("past");
-        } else if (blockhour === currentHour) {
-          $(this).removeClass("past");
-          $(this).addClass("present");
-        } else {
-          $(this).removeClass("present");
-          $(this).removeClass("past");
-          $(this).addClass("future");
-        }
-      });
-    }
-    hourUpdated();
   });
 });
+
+// var savedDisplay = localStorage.getItem("hour-9");
+// console.log(savedDisplay);
+
+function hourUpdated() {
+  //loop over time blocks
+  var currentHour = moment().hours();
+  $(timeBlockEl).each(function () {
+    //check if we have moved to the next hour
+    // Splits to hour, 9. The [1] is 9
+    var blockhour = parseInt($(this).attr("id").split("-")[1]); // hour-9: [hour, 9] [1,2,3,4,5]
+    console.log(this);
+    console.log(blockhour);
+    console.log(currentHour);
+
+    //Loads the saved data from local storage to the descrption text content
+    var hourID = $(this).attr("id");
+    console.log(hourID);
+    var savedDisplay = localStorage.getItem(hourID);
+    console.log(savedDisplay);
+    $(this).children(".description").text(savedDisplay);
+
+    //Color dispalys
+    if (blockhour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockhour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+hourUpdated();
